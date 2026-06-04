2b1198303d18dd37afab6f9fc6b09ae8fd0f187b
rjn45 <rajanayakaa52@gmail.com>
2026-06-04 13:18:48 +0530

    section-3 items cards added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Layout from "../../component/Layouts/Layout";
 import Section1 from './Section1';
 import "../../styles/HomeStyle.css";
 import Section2 from './Section2';
+import Section3 from './Section3';
 
 const Home = () => {
   return (
@@ -13,6 +14,9 @@ const Home = () => {
 
         {/* Home Section About */}
         <Section2 />
+
+        {/* Home Section Menu */}
+        <Section3 />
       </Layout>
     </>
   )
